2a58ccb2e87046d9fe2e766b7efe1fdbfdceeb8f
Michal Przysucha <michal.przysucha@allegro.com>
2025-12-12 22:23:46 +0100

    Day 6 extract method

--- day_6/src/main.rs
+++ day_6/src/main.rs
@@ -4,29 +4,7 @@ fn main() {
     // Part 2 only (Part 1 done in Excel)
     let mut sum_part_2 = 0;
     let file_name = "test_input.txt";
-    let mut sign_idxs: Vec<usize> = Vec::new();
-    let mut vertical_numbers_counter = 0;
-    for line in read_file(file_name) {
-        let line = line.unwrap();
-        vertical_numbers_counter = line.len();
-        if line.contains("+") || line.contains("*") {
-            for (idx, ch) in line.chars().enumerate() {
-                if ch == '+' || ch == '*' {
-                    sign_idxs.push(idx);
-                }
-            }
-            break;
-        }
-    }
-    let mut break_idxs: Vec<usize> = Vec::new();
-    for i in 1..sign_idxs.len() {
-        break_idxs.push(sign_idxs[i] - 1);
-    }
-    vertical_numbers_counter -= break_idxs.len();
-    let mut numbers: Vec<usize> = Vec::new();
-    for _ in 0..vertical_numbers_counter {
-        numbers.push(0);
-    }
+    let (mut numbers, break_idxs) = fetch_numbers_and_break_indices(file_name);
     for line in read_file(file_name) {
         let line = line.unwrap();
         let mut part_cnt = 0;
@@ -77,4 +55,31 @@ fn main() {
         }
     }
     println!("Part 2: {}", sum_part_2);
+}
+
+fn fetch_numbers_and_break_indices(file_name: &str) -> (Vec<usize>, Vec<usize>) {
+    let mut sign_idxs: Vec<usize> = Vec::new();
+    let mut vertical_numbers_counter = 0;
+    for line in read_file(file_name) {
+        let line = line.unwrap();
+        vertical_numbers_counter = line.len();
+        if line.contains("+") || line.contains("*") {
+            for (idx, ch) in line.chars().enumerate() {
+                if ch == '+' || ch == '*' {
+                    sign_idxs.push(idx);
+                }
+            }
+            break;
+        }
+    }
+    let mut break_idxs: Vec<usize> = Vec::new();
+    for i in 1..sign_idxs.len() {
+        break_idxs.push(sign_idxs[i] - 1);
+    }
+    vertical_numbers_counter -= break_idxs.len();
+    let mut numbers: Vec<usize> = Vec::new();
+    for _ in 0..vertical_numbers_counter {
+        numbers.push(0);
+    }
+    (numbers, break_idxs)
 }
